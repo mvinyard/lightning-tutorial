bad4080cb85fa740384761ea8c1399309c2c38be
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-09-13 17:30:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import re, os, sys
 
 setup(
     name="lightning-tutorial",
-    version="0.0.0",
+    version="0.0.1",
     python_requires=">3.7.0",
     author="Michael E. Vinyard - Harvard University - Massachussetts General Hospital - Broad Institute of MIT and Harvard",
     author_email="mvinyard@broadinstitute.org",
